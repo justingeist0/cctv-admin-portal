760e137cba6117bb09fe970e44e12465aeda3a6b
Justin Geist <justingeist0@gmail.com>
2024-03-11 21:38:28 -0400

    update firebase

--- src/AuthContext.js
+++ src/AuthContext.js
@@ -3,14 +3,23 @@ import { initializeApp } from "firebase/app";
 import { getAuth, onAuthStateChanged, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const AuthContext = createContext()
+// const firebaseConfig = {
+//     apiKey: "AIzaSyC8gcLnkEkvZ4osnSoUcBY1ZoizleHSLsw",
+//     authDomain: "adtv-64129.firebaseapp.com",
+//     projectId: "adtv-64129",
+//     storageBucket: "adtv-64129.appspot.com",
+//     messagingSenderId: "1051852504406",
+//     appId: "1:1051852504406:web:cfa882fc070b7de844aaf7"
+// };
 const firebaseConfig = {
-    apiKey: "AIzaSyC8gcLnkEkvZ4osnSoUcBY1ZoizleHSLsw",
-    authDomain: "adtv-64129.firebaseapp.com",
-    projectId: "adtv-64129",
-    storageBucket: "adtv-64129.appspot.com",
-    messagingSenderId: "1051852504406",
-    appId: "1:1051852504406:web:cfa882fc070b7de844aaf7"
-};
+    apiKey: "AIzaSyDdChm6HnT3sU99CGfEncHBfSRlU9lUPJY",
+    authDomain: "linklocal-c5d8f.firebaseapp.com",
+    projectId: "linklocal-c5d8f",
+    storageBucket: "linklocal-c5d8f.appspot.com",
+    messagingSenderId: "1081605742554",
+    appId: "1:1081605742554:web:ba6886c4fff72bfa1d2ae3",
+    measurementId: "G-ZQEN9XEX3R"
+  };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
